8bdb3d3d1abf1b44d2e2cfbe44b03a0610fbc5c0
sahand <sanaeisahand@gmail.com>
2023-01-10 15:06:05 +0330

    1.5 completed

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -1,5 +1,5 @@
 const Header = (props) => (
-    <h1>{props.course}</h1>
+    <h1>{props.course.name}</h1>
 )
 
 const Part = (props) => (
@@ -27,27 +27,29 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
       <Header course={course}/>
-      <Content parts={parts}/>
-      <Total exercises={parts}/>
+      <Content parts={course.parts}/>
+      <Total exercises={course.parts}/>
     </>
   )
 }
