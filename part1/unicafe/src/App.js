254bdd30c698e86b874c306a86435fd91644eb11
sahand <sanaeisahand@gmail.com>
2023-01-12 13:27:36 +0330

    1.8 completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 
 // button component
-const Button = (props) => {
+const Button = ({setState, state, text}) => {
   return (
     <>
-      <button onClick={() => props.setState(props.state + 1)}>
-        {props.text}
+      <button onClick={() => setState(state + 1)}>
+        {text}
       </button>
     </>
   ) 
@@ -13,33 +13,23 @@ const Button = (props) => {
 
 
 // display component
-const Display = (props) => {
+const Display = ({text, state, char}) => {
   return  (
     <>
-      <p>{props.text} {props.state} {props.char}</p>
+      <p>{text} {state} {char}</p>
     </>
   )
 }
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+// statistics component
+const Statistics = ({good, neutral, bad}) => {
   let all = good + neutral + bad;
   let score = good - bad
   let average = score/all || 0
   let positive = (good/all)*100 || 0
-  
 
   return (
     <>
-      <h1>give feedback</h1>
-      <div>
-        <Button setState={setGood} state={good} text={'good'} />
-        <Button setState={setNeutral} state={neutral} text={'neutral'} />
-        <Button setState={setBad} state={bad} text={'bad'} />
-      </div>
       <h1>statistics</h1>
       <div>
         <Display state={good} text={'good'} />
@@ -49,7 +39,25 @@ const App = () => {
         <Display state={average} text={'average'} />
         <Display state={positive} text={'positive'} char={'%'}/>
       </div>
-      
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+  return (
+    <>
+      <h1>give feedback</h1>
+      <div>
+        <Button setState={setGood} state={good} text={'good'} />
+        <Button setState={setNeutral} state={neutral} text={'neutral'} />
+        <Button setState={setBad} state={bad} text={'bad'} />
+      </div>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </>
   )
 }
