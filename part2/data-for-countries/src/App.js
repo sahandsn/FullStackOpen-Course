4b1a227071efffe238fa5b9531a25ec373c08442
sahand <sanaeisahand@gmail.com>
2023-01-16 16:51:35 +0330

    initializing data for countries

---
+++ part2/data-for-countries/src/App.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from 'react'
+import axios from "axios"
+
+function App() {
+  return (
+    <p>something great is being built here ...</p>
+  );
+}
+
+export default App;
