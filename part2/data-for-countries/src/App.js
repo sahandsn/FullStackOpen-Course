9e45aec677871a19c3bf9adf48aaf021a18aa37a
sahand <sanaeisahand@gmail.com>
2023-01-17 15:54:25 +0330

    2.14 completed

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -18,6 +18,7 @@ const CountryFinder = ({countries, search, setSearch}) => {
       <></>
     )
   }
+
   // started typing
   const matchedCountries = countries.filter((obj)=>{
     const regex = new RegExp(search, "i")
@@ -47,6 +48,7 @@ const CountryFinder = ({countries, search, setSearch}) => {
     // console.log(event.target.value);
     setSearch(event.target.value)
   }
+
   // [2,10] countries matched this search
   if(matchedCountries.length<=10 && matchedCountries.length>=2){
     return (
@@ -55,7 +57,7 @@ const CountryFinder = ({countries, search, setSearch}) => {
           {matchedCountries.map(obj=>{
             // console.log(obj.name.common);
             return (
-              <li key={obj.cca3}>{obj.name.common}; {obj.name.official} <button value={obj.name.common} onClick={CountryProfileHandler}>show</button></li>
+              <li key={obj.cca3}>{obj.name.common}; {obj.name.official} <button value={obj.name.official} onClick={CountryProfileHandler}>show</button></li>
             )
           })}
         </ul>
@@ -71,36 +73,86 @@ const CountryFinder = ({countries, search, setSearch}) => {
   }
 }
 
+
+const CurrentWeatherCity = ({city, countryCode}) => {
+  const [weather, setWeather] = useState("CALCULATING")
+  const [weatherIcon, setWeatherIcon] = useState('')
+  const [weatherIconCaption, setWeatherIconCaption] = useState('')
+
+  useEffect(()=>{
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
+      .then(response=>{
+        // console.log(response.data)
+        setWeather(response.data.main.temp)
+        setWeatherIcon(response.data.weather[0].icon)
+        setWeatherIconCaption(response.data.weather[0].description)
+      })
+      .catch(err => console.warn(err))
+      
+  }, [city, countryCode])
+
+  if(weatherIconCaption!==""){
+    return(
+      <div>
+        <p>Temperature(&#8451;): {weather}</p>
+        <figure>
+          <img src={`https://openweathermap.org/img/wn/${weatherIcon}.png`} alt={'weather icon'} />
+          <figcaption>{weatherIconCaption}</figcaption>
+        </figure>
+      </div>
+      
+    )
+  }
+}
+
+
 const CountryProfile = ({country}) => {
+  // console.log(country);
   return (
     <>
-        <h2>{country.name.common}</h2>
+      <h2>{country.name.common}</h2>
+      <figure>
+        <img alt={country.name.common} src={country.flags.png} />
+        <figcaption>{country.name.official}</figcaption>
+      </figure>
 
-        <figure>
-          <img alt={country.name.common} src={country.flags.png} loading="eager" />
-          <figcaption>{country.name.official}</figcaption>
-        </figure>
-
-        <h3>Geography</h3>
-        <ul>
+      <h3>Geography</h3>
+      <ul>
+        <div>
           <b>Capital(s):</b>
             <ul>
-              {country.capital.map((city)=><li key={city}>{city}</li>)}
-            </ul>
-          <p><b>Area(km<sup>2</sup>):</b> {new Intl.NumberFormat().format(country.area)}</p>
-        </ul>
-        
-        <h3>Anthropology</h3>
-        <ul>
-        <b>Language(s):</b>
-          <ul>
-            {Object.keys(country.languages).map((lang)=><li key={lang}>{country.languages[lang]}</li>)}
+              {country.capital.map((city)=>{
+                return (
+                  <div key={city}>
+                    <li>{city}</li>
+                    <CurrentWeatherCity city={city} countryCode={country.cca3}/>
+                  </div>
+                )
+              })}
           </ul>
+        </div>
+        <div>
+          <p><b>Area(km<sup>2</sup>):</b> {new Intl.NumberFormat().format(country.area)}</p>
+        </div>
+      </ul>
+        
+      <h3>Anthropology</h3>
+      <ul>
+        <div>
+          <b>Language(s):</b>
+          <ul>
+              {Object.keys(country.languages).map((lang)=><li key={lang}>{country.languages[lang]}</li>)}
+          </ul>
+        </div>
+        <div>
           <p><b>Population:</b> {new Intl.NumberFormat().format(country.population)}</p>
-        </ul>
-      </>
+        </div>
+      </ul>
+    </>
   )
 }
+
 
 function App() {
 
@@ -115,6 +167,7 @@ function App() {
     axios 
       .get("https://restcountries.com/v3.1/all")
       .then(response=>setCountries(response.data))
+      .catch(error=>console.warn(error))
   },[])
   // console.log(countries)
   
