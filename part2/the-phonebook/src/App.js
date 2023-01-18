ad02b938845fb02ab0e813797dce7706d8c41835
sahand <sanaeisahand@gmail.com>
2023-01-18 15:32:21 +0330

    data-for-countries exercise was moved to a later section, so 2.11 is the latest exercise in the current order

--- part2/the-phonebook/src/App.js
+++ part2/the-phonebook/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from "axios"
 
+
 const Search = ({persons, newSearchValue, searchOnChange}) => {
   if (newSearchValue===""){
     return(
@@ -149,7 +150,6 @@ const App = () => {
     </>
   )
 }
-
 
 
 export default App
