3357756559e73e86149f5b210d03752ae087f794
sahand <sanaeisahand@gmail.com>
2023-01-23 17:10:16 +0330

    nodemon and express

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,35 @@
-const http = require("http")
-const app = http.createServer((req,res)=>{
-    res.writeHead(200, {"Content-Type":"text/plain"})
-    res.end("Hello World")
+const express = require("express")
+
+
+const app = express()
+
+
+const notes = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+app.get("/api/notes",(req,res)=>{
+    res.json(notes)
 })
 
 const PORT = 3001
