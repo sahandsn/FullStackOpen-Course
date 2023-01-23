b8f0df493b7ccba81bdf4809cd1dd4b8349be2d1
sahand <sanaeisahand@gmail.com>
2023-01-23 16:42:12 +0330

    initializing part3

---
+++ part3/phonebook-backend/index.js
@@ -0,0 +1,8 @@
+const http = require("http")
+const app = http.createServer((req,res)=>{
+    res.writeHead(200, {"Content-Type":"text/plain"})
+    res.end("Hello World")
+})
+
+const PORT = 3001
+app.listen(PORT)
