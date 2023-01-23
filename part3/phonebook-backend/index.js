27cc6d44f27c4af5417081047795a7f193ea1498
sahand <sanaeisahand@gmail.com>
2023-01-23 18:05:33 +0330

    3.4 completed

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -4,10 +4,10 @@ const express = require("express")
 const app = express()
 
 
-const notes = [
+let notes = [
     { 
       "id": 1,
-      "name": "Arto Hellas", 
+      "name": "sahand", 
       "number": "040-123456"
     },
     { 
@@ -48,6 +48,14 @@ app.get("/api/persons/:id", (req, res)=>{
     else{
         res.status(404).send('Not Found!')
     }
+})
+
+
+app.delete("/api/persons/:id", (req,res)=>{
+    const id = Number(req.params.id)
+    notes = notes.filter(n=>n.id!==id)
+    res.status(204)
+    
 })
 
 
