b4d41f0d474573b9ca4ac5f831d38e675b604f02
sahand <sanaeisahand@gmail.com>
2023-02-06 19:40:17 +0330

    4.4 completed

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,5 +1,15 @@
 const dummy = () => 1;
 
+const totalLikes = (blogsArr) => {
+  if (blogsArr.length === 0) {
+    console.log(0);
+    return 0;
+  }
+  console.log(blogsArr.reduce((total, current) => total + current.likes, 0));
+  return blogsArr.reduce((total, current) => total + current.likes, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
