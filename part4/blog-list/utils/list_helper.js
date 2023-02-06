3cabbc6406f97f72f884f611c118f8371993fc74
sahand <sanaeisahand@gmail.com>
2023-02-06 20:39:11 +0330

    4.5 completed

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,14 +2,27 @@ const dummy = () => 1;
 
 const totalLikes = (blogsArr) => {
   if (blogsArr.length === 0) {
-    console.log(0);
+    // console.log(0);
     return 0;
   }
-  console.log(blogsArr.reduce((total, current) => total + current.likes, 0));
+  // console.log(blogsArr.reduce((total, current) => total + current.likes, 0));
   return blogsArr.reduce((total, current) => total + current.likes, 0);
+};
+
+const favoriteBlog = (blogsArr) => {
+  if (blogsArr.length === 0) {
+    return {};
+  }
+  return blogsArr.reduce((mostLiked, current) => {
+    if (current.likes > mostLiked.likes) {
+      return current;
+    }
+    return mostLiked;
+  }, { likes: -1 });
 };
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
