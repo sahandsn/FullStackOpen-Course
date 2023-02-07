5a5ae22ce6b3c3fbc90c5f177e746b6fb3412bc1
sahand <sanaeisahand@gmail.com>
2023-02-08 00:28:33 +0330

    4.6 completed

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-prototype-builtins */
+// const _ = require('lodash');
+
 const dummy = () => 1;
 
 const totalLikes = (blogsArr) => {
@@ -21,8 +25,28 @@ const favoriteBlog = (blogsArr) => {
   }, { likes: -1 });
 };
 
+const mostBlogs = (blogsArr) => {
+  if (blogsArr.length === 0) {
+    return {};
+  }
+  const newObj = blogsArr.reduce((objs, currentObj) => {
+    if (!objs.hasOwnProperty(currentObj.author)) {
+      objs[currentObj.author] = 0;
+    }
+    objs[currentObj.author] += 1;
+    return objs;
+  }, {});
+  return Object.keys(newObj).reduce((most, current) => {
+    if (most.blogs < newObj[current]) {
+      return { author: current, blogs: newObj[current] };
+    }
+    return most;
+  }, { author: 'sahand', blogs: -1 });
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
