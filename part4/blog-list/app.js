ab51bf1de1fd51dcea462b0168894afeb10e64b0
sahand <sanaeisahand@gmail.com>
2023-02-15 00:18:13 +0330

    4.19 completed

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const config = require('./utils/config');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 const { info, error } = require('./utils/logger');
 
@@ -27,6 +28,7 @@ app.use(morgan(':method :url :status :res[content-length] -> :response-time ms <
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpont);
 app.use(middleware.errorHandler);
