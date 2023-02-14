f7ce392dbb77326d67c88e699f571ca2bf303ad2
sahand <sanaeisahand@gmail.com>
2023-02-14 23:17:29 +0330

    4.18 completed

--- part4/blog-list/utils/config.js
+++ part4/blog-list/utils/config.js
@@ -5,4 +5,6 @@ const MONGODB_URI = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI;
 
-module.exports = { PORT, MONGODB_URI };
+const { SECRETE } = process.env;
+
+module.exports = { PORT, MONGODB_URI, SECRETE };
