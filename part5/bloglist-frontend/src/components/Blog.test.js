3a2090f13137edc3bcd0792c66fdb2d3b4a7f24f
sahand <sanaeisahand@gmail.com>
2023-03-10 00:35:34 +0330

    5.14 completed

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import userEvent from '@testing-library/user-event'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
@@ -16,16 +17,16 @@ import Blog from './Blog'
 // })
 
 describe('Blog component tests', () => {
+  const user = {
+    name: 'sahand',
+    username: 'sahandsn',
+  }
   const blog = {
     title: 'a title',
     author: 'an author',
     url: 'a url',
     likes: 1,
-  }
-
-  const user = {
-    name: 'sahand',
-    username: 'sahandsn',
+    user: user,
   }
 
   const mockUpdateBlog = jest.fn()
@@ -43,5 +44,21 @@ describe('Blog component tests', () => {
     expect(container).toHaveTextContent('a title an author')
     expect(container).not.toHaveTextContent('a url')
     expect(container).not.toHaveTextContent('Likes: 1')
+  })
+  test('likes and url are shown after button is clicked', async () => {
+    const { container } = render(
+      <Blog
+        blog={blog}
+        handleLike={mockUpdateBlog}
+        handleDelete={mockDeleteBlog}
+        user={user}
+      />
+    )
+    const mockUser = userEvent.setup()
+    const button = screen.getByText('View')
+    await mockUser.click(button)
+    expect(container).toHaveTextContent('a title by an author')
+    expect(container).toHaveTextContent('a url')
+    expect(container).toHaveTextContent('Likes: 1')
   })
 })
