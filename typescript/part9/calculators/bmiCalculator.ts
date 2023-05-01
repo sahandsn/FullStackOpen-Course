071b414605ba6015023403da14edd19b838cac04
sahand <sanaeisahand@gmail.com>
2023-05-01 16:14:19 +0430

    9.6 completed

--- typescript/part9/calculators/bmiCalculator.ts
+++ typescript/part9/calculators/bmiCalculator.ts
@@ -1,52 +1,52 @@
 interface bmiType {
-  weight: number
-  height: number
+  weight: number;
+  height: number;
 }
-const parseArgs = (args: string[]): bmiType => {
+export const parseArgs = (args: string[]): bmiType => {
   if (args.length !== 4) {
     throw new Error(
       'incorrect input. try: npm run bmiCalculator.ts weight height'
-    )
+    );
   }
   if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
     return {
       weight: Number(args[2]),
       height: Number(args[3]),
-    }
+    };
   } else {
     throw new Error(
       'incorrect input. try: npm run bmiCalculator.ts weight height'
-    )
+    );
   }
-}
+};
 
 export const calculateBmi = (heightCM: number, weightKG: number) => {
-  const score = weightKG / (heightCM / 100) ** 2
+  const score = weightKG / (heightCM / 100) ** 2;
   if (score < 18.5) {
-    return 'Under weight'
+    return 'Under weight';
   } else if (18.5 < score && score < 24.9) {
-    return 'Normal(healthy weight)'
+    return 'Normal(healthy weight)';
   } else if (25 < score && score < 29.9) {
-    return 'Normal(healthy weight)'
+    return 'Normal(healthy weight)';
   } else if (30 < score && score < 34.9) {
-    return 'Obesity(class I)'
+    return 'Obesity(class I)';
   } else if (35 < score && score < 39.9) {
-    return 'Obesity(class II)'
+    return 'Obesity(class II)';
   } else if (40 < score) {
-    return 'Exreme obesity'
+    return 'Exreme obesity';
   } else {
-    return 'sth went wrong.'
+    return 'sth went wrong.';
   }
-}
+};
 
-try {
-  const { weight, height } = parseArgs(process.argv)
-  const msg = calculateBmi(height, weight)
-  console.log(msg)
-} catch (e: unknown) {
-  let errMsg = 'sth went wrong.'
-  if (e instanceof Error) {
-    errMsg += e.message
-  }
-  console.log(errMsg)
-}
+// try {
+//   const { weight, height } = parseArgs(process.argv);
+//   const msg = calculateBmi(height, weight);
+//   console.log(msg);
+// } catch (e: unknown) {
+//   let errMsg = 'sth went wrong.';
+//   if (e instanceof Error) {
+//     errMsg += e.message;
+//   }
+//   console.log(errMsg);
+// }
---
+++ typescript/part9/calculators/exerciseCalculator.ts
@@ -0,0 +1,81 @@
+interface exerciseType {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+interface exerciseDataType {
+  data: number[];
+  target: number;
+}
+
+export const parseArgsArr = (args: string[]): exerciseDataType => {
+  if (args.length < 4) {
+    throw new Error(
+      'incorrect input. try: npm run bmiCalculator.ts target data1, ...'
+    );
+  }
+  const [,, target, ...data] = args;
+  const dataArr = data.map((d) => Number(d));
+
+  if (!isNaN(Number(target)) && dataArr.every((d) => !isNaN(d))) {
+    return {
+      data: dataArr,
+      target: Number(target),
+    };
+  } else {
+    throw new Error(
+      'incorrect input. try: npm run bmiCalculator.ts target data1, ...'
+    );
+  }
+};
+
+export const calculateExercises = (data: number[], target: number): exerciseType => {
+  const average =
+    data.reduce((total, current) => {
+      return total + current;
+    }, 0) / data.length;
+
+  const rating = average === target ? 3 : average >= target / 2 ? 2 : 1;
+
+  let ratingDescription;
+  switch (rating) {
+    case 3:
+      ratingDescription = 'congradulations! you reached your goal.';
+      break;
+    case 2:
+      ratingDescription = 'not too bad but could be better.';
+      break;
+    default:
+      ratingDescription = 'it is ok if you felt lazy a little.';
+      break;
+  }
+
+  const res = {
+    periodLength: data.length,
+    trainingDays: data.filter((d) => d !== 0).length,
+    success: average === target,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+
+  return res;
+};
+
+// try {
+//   const { data, target } = parseArgsArr(process.argv);
+//   const res = calculateExercises(data, target);
+//   console.log(res);
+// } catch (e: unknown) {
+//   let errMsg = 'sth went wrong.';
+//   if (e instanceof Error) {
+//     errMsg += e.message;
+//   }
+//   console.log(errMsg);
+// }
