7a3cf532a27221f034d451964f55df232978b863
sahand <sanaeisahand@gmail.com>
2023-05-07 21:32:33 +0430

    9.22 completed

--- typescript/part9/patientor/backend/src/types/patients.ts
+++ typescript/part9/patientor/backend/src/types/patients.ts
@@ -1,3 +1,5 @@
+import { DiagnosisType as Diagnosis } from './diagnosis';
+
 export enum Gender {
   Male = 'male',
   Female = 'female',
@@ -13,6 +15,45 @@ export type PatientType = {
   occupation: string;
   entries: Entry[];
 };
-export interface Entry {}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 export type NonSeneitivePatientType = Omit<PatientType, 'ssn'|'entries'>;
 export type NewPatientType = Omit<PatientType, 'id' | 'entries'>;
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  Healthy,
+  LowRisk,
+  HighRisk,
+  CriticalRisk
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  sickLeave?: {
+    startDate: string,
+    endDate: string,
+  };
+  employerName?: string
+}
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string,
+    criteria: string,
+  };
+}
