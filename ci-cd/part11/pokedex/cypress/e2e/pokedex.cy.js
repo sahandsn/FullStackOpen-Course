028b5d1c6603dec4e7f90e999cd269caa27c3322
sahand <sanaeisahand@gmail.com>
2023-06-02 16:50:08 +0330

    11.9 completed

--- ci-cd/part11/pokedex/cypress/e2e/pokedex.cy.js
+++ ci-cd/part11/pokedex/cypress/e2e/pokedex.cy.js
@@ -1,7 +1,17 @@
-describe('Pokedex', function() {
-  it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+describe('Pokedex', function () {
+  beforeEach(function () {
+    cy.visit('http://localhost:5000');
+  });
+  it('front page can be opened', function () {
+    cy.contains('ivysaur');
+    cy.contains(
+      'Pokémon and Pokémon character names are trademarks of Nintendo.'
+    );
+  });
+  it('main page tp ivysaur pokemon page', function () {
+    cy.contains('ivysaur').click();
     cy.contains('ivysaur')
-    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
-  })
-})
+    cy.contains('chlorophyll')
+    cy.contains('overgrow');
+  });
+});
